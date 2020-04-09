e4d9d9988b0bfe585ae2e8dd5ed1ca2a55a0cc17
eisuto <waabdxiaoyi@gmail.com>
2020-04-09 18:48:21 +0800

    index up

--- models/video.go
+++ models/video.go
@@ -8,9 +8,10 @@ type Video struct {
 	gorm.Model
 	User      User
 	UserID    int    `gorm:"index"`
-	Title     string `gorm:"size:64"`
-	Like      int    `gorm:"default:0"`
-	UnLike    int    `gorm:"default:0"`
+	FCUrl     string `gorm:"size:64"`   //封面地址
+	Title     string `gorm:"size:64"`   //标题
+	Like      int    `gorm:"default:0"` //赞
+	UnLike    int    `gorm:"default:0"`	//踩
 	Introduc  string `gorm:"size:200"`	//简介
 	duration  int    					//时长
 }
