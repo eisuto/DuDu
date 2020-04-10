c1ea4d23e1b38ca2c1d771cba9bea2b4c439980b
eisuto <waabdxiaoyi@gmail.com>
2020-04-10 21:20:05 +0800

    视频封面

--- models/core.go
+++ models/core.go
@@ -14,7 +14,7 @@ func Init() {
 	//return db
 	db.AutoMigrate(&User{})
 	db.AutoMigrate(&Video{})
-	var count int
+	/*var count int
 	// Model(&User{})查询用户表, Count(&count) 将用户表的数据赋值给count字段。
 	if err := db.Model(&User{}).Count(&count).Error; err == nil && count == 0 {
 		//新增
@@ -28,7 +28,21 @@ func Init() {
 			//角色 管理员
 			Role: 0,
 		})
-	}
+	}*/
+	/*eisuto := GetUserByCard("6")
+	//新增视频
+	db.Create(&Video{
+		User:eisuto,
+		FCUrl:"./static/img/fm/1",
+		Title:"幻灯花",
+		Play:1,
+		Like:0,
+		UnLike:0,
+		Introduc:"一曲幻灯花不仅粉碎了原曲，也几乎粉碎了听众的心，尽管男性的声音在东方歌姬中比例不大，" +
+			"然而这首绝对为精品预定，略带沧桑的男声不会让你一耳朵惊艳，然而听过，才会明白这是首多么耐听的歌",
+		duration:378,
+	})
+	*/
 }
 
 
--- models/video.go
+++ models/video.go
@@ -10,6 +10,7 @@ type Video struct {
 	UserID    int    `gorm:"index"`
 	FCUrl     string `gorm:"size:64"`   //封面地址
 	Title     string `gorm:"size:64"`   //标题
+	Play	  int    `gorm:"size:32"`   //播放数
 	Like      int    `gorm:"default:0"` //赞
 	UnLike    int    `gorm:"default:0"`	//踩
 	Introduc  string `gorm:"size:200"`	//简介
