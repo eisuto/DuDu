2010b24dc2446cf1dccf3605b32e4d8c8779eab1
eisuto <waabdxiaoyi@gmail.com>
2020-04-13 22:09:43 +0800

    index-up

--- static/js/idnex.js
+++ static/js/idnex.js
@@ -47,3 +47,30 @@ function get_12video(){
         }
     });
 }
+//获取指定分区首页视频
+function get_area_video(){
+    $.ajax({
+        async: false,
+        type:"POST",
+        url:"/get_index_animeVideo",
+        //data:{"card":card,"pwd":pwd}
+        }).done(function(msg){
+        if(msg != null){
+            console.log("回调推荐视频成功")
+            console.log(msg.vids)
+            for(var i=0;i<msg.vids.length;i++){
+                $('#12v').append(
+                    "<div class='pr-video'>"+
+                    "<img src='"+msg.vids[i].FCUrl+"' alt=''>"+
+                    "<p id='big-video-title'>"+msg.vids[i].Title+"</p>"+
+                    "<a href='./static/img/video/"+msg.vids[i].ID+".mp4'><div class='mask'>"+
+                        "<p id='mask-title'>"+msg.vids[i].Title+"</p>"+
+                        "<p id='mask-up'>UP: eisuto</p>"+
+                        "<p id='mask-click'><i class='glyphicon glyphicon-expand'></i>"+msg.vids[i].Play+"</p>"+
+                    "</div></a>"+
+                    "</div>"
+                )
+            }
+        }
+    });
+}
